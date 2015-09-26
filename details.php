248a8c9b2d131ef7c8ae218441cc88a58f637a81
Chen Wei <weichen302@gmail.com>
2015-09-26 21:23:09 +0200

    move jquery-1.10.1.min.js to localhost

--- details.php
+++ details.php
@@ -5,7 +5,7 @@
 	<title>BandWidthd</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	<script type="text/javascript" src="js/jquery-1.10.1.min.js"></script>
 	<script type="text/javascript" src="js/jquery.tinysort.min.js"></script>
 	<script type="text/javascript" src="js/bandwidthd.js"></script>
 	<link media="screen" rel="stylesheet" type="text/css" href="css/style.css">
